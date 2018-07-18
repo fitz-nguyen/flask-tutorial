6550b11cf9a1d2c21c52d3b874bb8ffb12258444
Nguyen Tuong <nguyentuong@Nguyens-MacBook-Pro.local>
2018-07-18 14:54:52 +0700

    add auth

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -21,8 +21,10 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    # a simple page thar says hello
     from . import db
     db.init_app(app)
 
+    from . import auth
+    app.register_blueprint(auth.bp)
+    
     return app
